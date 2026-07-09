81564bc090563a1ac781b306fb16a21a1299c2e1
Jerry Lin <jerry@docspot.com>
2026-07-09 12:25:33 -0700

    Added method to check if a visitor is part of a visitor group

---
+++ src/main/java/com/nettoolkit/gatekeeper/CheckVisitorGroupMembership.java
@@ -0,0 +1,61 @@
+package com.nettoolkit.gatekeeper;
+
+import java.util.UUID;
+import java.time.OffsetDateTime;
+import java.time.Instant;
+import com.nettoolkit.exception.ParsingException;
+import com.nettoolkit.exception.NetToolKitException;
+import com.nettoolkit.gatekeeper.GatekeeperClient;
+import com.nettoolkit.json.JSONException;
+import com.nettoolkit.json.JSONObject;
+import com.nettoolkit.internal.ApiResponse;
+import com.nettoolkit.internal.http.HttpContentType;
+import com.nettoolkit.internal.request.GetRequest;
+
+/**
+ * <strong>EXPERIMENTAL</strong>: This route is not guaranteed to have stable performance yet. The API is also subject to change, based on supportable queries.
+ */
+public class CheckVisitorGroupMembership extends GetRequest {
+    private UUID mVisitorGroupId = null;
+    private String mstrVisitor = null;
+
+    public CheckVisitorGroupMembership(GatekeeperClient client) {
+        super(client);
+    }
+
+    @Override
+    protected String getPath() {
+        return "/v1/gatekeeper/visitor-groups/" + mVisitorGroupId
+            + "/membership/" + mstrVisitor;
+    }
+
+    public CheckVisitorGroupMembership visitorGroupId(UUID visitorGroupId) {
+        mVisitorGroupId = visitorGroupId;
+        return this;
+    }
+
+    public CheckVisitorGroupMembership visitor(String strVisitor) {
+        mstrVisitor = strVisitor;
+        return this;
+    }
+
+    /**
+     * Sends the request.
+     *
+     * @return the visit count for IP address as seen by policy
+     * @throws NetToolKitException
+     */
+    public boolean send() throws NetToolKitException {
+        ApiResponse response = getClient().send(this);
+        JSONObject jsonResult = response.getFirstResult();
+        JSONObject jsonMembership = jsonResult.optJSONObject("membership");
+        if (jsonMembership != null) {
+            String strIsMember = jsonMembership.optString("is_member");
+            if (strIsMember != null) {
+                return strIsMember.equalsIgnoreCase("true");
+            }
+        }
+        throw new NetToolKitException("Missing or invalid response from NetToolKit");
+    }
+}
+
